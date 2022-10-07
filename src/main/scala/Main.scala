0788ee81e01da03b158801dace4253dfce6eba47
Vivian Berger <vivianberger028@gmail.com>
2022-10-07 10:12:20 +0200

    Implemented basic calculator.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,46 @@
 
 @main
 def main(): Unit = {
-  println("Hello World")
+
+  val text =
+    """
+    |Pick one:
+    |1: Addition
+    |2: Subtraction
+    |3: Multiplication
+    |4: Division
+    |5: Exit
+    """.stripMargin
+
+  var keepRunning = true
+  while (keepRunning) {
+  println(text)
+
+  val operation = Console.in.readLine().toInt
+    if (operation < 5 && operation > 0) {
+      println("Enter x:")
+      val x = Console.in.readLine().toInt
+      println("Enter y")
+      val y = Console.in.readLine().toInt
+
+      if (operation == 1) {
+        println("Result:" + (x + y))
+      }
+      else if (operation == 2) {
+        println("Result:" + (x - y))
+      }
+      else if (operation == 3) {
+        println("Result:" + (x * y))
+      }
+      else if (operation == 4) {
+        println("Result:" + (x / y))
+      }
+    }
+    else if (operation == 5) {
+      keepRunning = false
+    }
+    else {
+      println("try again")
+    }
+  }
 }
